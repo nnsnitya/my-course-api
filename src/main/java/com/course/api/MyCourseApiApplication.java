9437558f66cbff9c2a9dc220b8dc83c8e83c63ba
Nityanand Singh <nns.nitya@gmail.com>
2026-05-06 16:29:31 +0530

    Initial commit

---
+++ src/main/java/com/course/api/MyCourseApiApplication.java
@@ -0,0 +1,13 @@
+package com.course.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCourseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/api/MyCourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
